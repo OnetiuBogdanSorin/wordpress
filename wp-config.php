dd123a02833001c1831abae399ec4bf08d47cfc8
Bogdan <sbogdan37@yahoo.com>
2021-04-08 22:39:01 +0300

    Database problem solved

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DB_NAME', 'WordPress' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'ddnfp6k5' );
+define( 'DB_PASSWORD', '' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
--- wp-content/plugins/plug/shortcodes.php
+++ wp-content/plugins/plug/shortcodes.php
@@ -1,24 +1,24 @@
 <?php
 
 /**
- * Plugin Name: plug                                                    a=5,b=7
+ * Plugin Name: plug
  * Description: Just a plugin
  */
 function recent_posts_function($atts)
 {
     extract(shortcode_atts(array('id' => 3), $atts));
     global $wpdb;
-    $post=$wpdb->get_results("SELECT * FROM $wpdb->postView", OBJECT);
-    //$post=get_post(1);
+    //$posts=$wpdb->get_results($wpdb->prepare("SELECT ID,post_title, post_name FROM $wpdb->posts WHERE id=$id", OBJECT));
+    $post = $wpdb->get_results("SELECT ID, post_title, post_status FROM `postview` WHERE id = $id", OBJECT);
     var_dump($post);
     $return_string = '<table><tr>
             <th>ID</th>
             <th>Title of the post</th>
-            <th>Name of the post</th>
+            <th>Status of the post</th>
             </tr>';
     foreach($post as $print)
     {
-        $return_string.='<tr><td>'.$print->ID.'</td><td>'.$print->post_title.'</td><td>'.$print->post_name.'</td></tr>';
+        $return_string .="<tr><td>$print->ID</td><td>$print->post_title</td><td>$print->post_status</td></tr>";
     }
     $return_string .= '</table>';
     return $return_string;
